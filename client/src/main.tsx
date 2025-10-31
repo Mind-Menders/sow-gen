94bb0845c6e3e50d5258450b6a715a51fda9cc8c
harryviswa <harry.viswa@gmail.com>
2025-10-31 08:30:25 +0400

    fix: Add polyfill for getRandomValues in browser for compatibility

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,8 @@
+// Polyfill for getRandomValues in browser (for some libraries that expect it)
+if (typeof window !== "undefined" && (!window.crypto || !window.crypto.getRandomValues)) {
+	// @ts-ignore
+	window.crypto = require("crypto").webcrypto;
+}
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
